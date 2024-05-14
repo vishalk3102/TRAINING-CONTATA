f9787b0ad9d5790a53179ffb3dc808af2fa7bcc6
vishal kumar <vishal.k3102@gmail.com>
2024-05-14 16:52:45 +0530

    code updated

--- .NET/EmployeeTax/server/server/obj/Debug/net8.0/server.AssemblyInfo.cs
+++ .NET/EmployeeTax/server/server/obj/Debug/net8.0/server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+567d91fea6178701852c3b73510c7bd119225b62")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae8d17bc2c2302df35d37f7dcd5c81b82c125cc1")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
